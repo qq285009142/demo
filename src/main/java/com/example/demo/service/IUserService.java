0dd9915d9915ab3fb5b0597f5cf3979808407fc5
dengjinglei <285009142@qq.com>
2019-06-28 11:50:11 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -0,0 +1,5 @@
+package com.example.demo.service;
+
+public interface IUserService {
+    public String getPwdByName(String unm);
+}
---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+    @Override
+    public String getPwdByName(String unm) {
+        return null;
+    }
+}
